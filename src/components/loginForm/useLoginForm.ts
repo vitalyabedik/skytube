0dd38ef9facd7c7c367fd0d428939105eec9b5ae
Vitali Bedik <vitalya.bedik@gmail.com>
2023-11-15 01:08:33 +0300

    feat: add useLoginForm hook

---
+++ src/components/loginForm/useLoginForm.ts
@@ -0,0 +1,23 @@
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const loginSchema = z.object({
+  password: z
+    .string()
+    .min(6, 'Use 6 characters or more for your password!')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter'),
+  username: z.string().min(1, 'Name is required!'),
+})
+
+export type LoginFormValues = z.infer<typeof loginSchema>
+
+export const useLoginForm = () =>
+  useForm<LoginFormValues>({
+    defaultValues: {
+      password: '',
+      username: '',
+    },
+    resolver: zodResolver(loginSchema),
+  })
