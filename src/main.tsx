641d1b762f9eb291ed57abfee2d64d6de1e91a7d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-11-15 01:06:53 +0300

    feat: add reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { App } from '@/app'
 import { createRoot } from 'react-dom/client'
 
+import '@/styles/index.scss'
+
 createRoot(document.getElementById('root') as HTMLElement).render(<App />)
