be0723ec1c18f536b7e9ae44b8a719b895543313
Vitali Bedik <vitalya.bedik@gmail.com>
2023-11-19 10:40:04 +0300

    feat: add CustomHeader component, styles for Page

--- src/components/customHeader/CustomHeader.tsx
+++ src/components/customHeader/CustomHeader.tsx
@@ -1,5 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import HeaderLogo from '@/assets/images/logo/HeaderLogo'
+import { Route } from '@/common'
+import { Page } from '@/components'
+import { Menu } from 'antd'
+import Button from 'antd/lib/button'
+import Flex from 'antd/lib/flex'
+
+import s from './CustomHeader.module.scss'
 
 export const CustomHeader: React.FC = () => {
-  return <div>CustomHeader</div>
+  const menuItems = [
+    { id: 1, path: Route.Search, title: 'Поиск' },
+    { id: 2, path: Route.Favorites, title: 'Избранное' },
+  ]
+
+  return (
+    <Page>
+      <Flex align={'center'} justify={'space-between'}>
+        <Flex align={'center'}>
+          <Link className={s.link} to={Route.Main}>
+            <HeaderLogo />
+          </Link>
+          <Menu
+            defaultSelectedKeys={['1']}
+            items={menuItems.map(menuItem => {
+              const key = menuItem.id
+
+              return {
+                key,
+                label: <Link to={menuItem.path}>{menuItem.title}</Link>,
+              }
+            })}
+            mode={'horizontal'}
+            theme={'light'}
+          />
+        </Flex>
+        <Button>Выйти</Button>
+      </Flex>
+    </Page>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
+import { Provider } from 'react-redux'
+
 import { App } from '@/app'
+import { store } from '@/app/model/store'
 import { createRoot } from 'react-dom/client'
 
 import '@/styles/index.scss'
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
