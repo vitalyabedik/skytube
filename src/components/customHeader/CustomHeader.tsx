d58f18cbc4b5f9c572d8597963a069f42587c4bb
Vitali Bedik <vitalya.bedik@gmail.com>
2023-12-05 20:44:20 +0300

    feat: add correct active menu item presentation and clear data after logout

--- src/components/customHeader/CustomHeader.tsx
+++ src/components/customHeader/CustomHeader.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 import HeaderLogo from '@/assets/images/logo/HeaderLogo'
 import { Route, useAppDispatch } from '@/common'
 import { Page } from '@/components'
-import { authActions } from '@/features'
-import { Menu } from 'antd'
+import { authActions, searchActions } from '@/features'
 import Button from 'antd/lib/button'
 import Flex from 'antd/lib/flex'
+import Menu from 'antd/lib/menu'
 
 import s from './CustomHeader.module.scss'
 
 export const CustomHeader: React.FC = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(['1']) // Используйте useState для отслеживания выбранного пункта меню
+
+  useEffect(() => {
+    const pathname = location.pathname
+    const matchingMenuItem = menuItems.find(menuItem => pathname.startsWith(menuItem.path))
+
+    if (matchingMenuItem) {
+      setSelectedKeys([matchingMenuItem.id.toString()])
+    } else {
+      setSelectedKeys([])
+    }
+  }, [location.pathname])
 
   const menuItems = [
     { id: 1, path: Route.Search, title: 'Поиск' },
@@ -22,6 +35,8 @@ export const CustomHeader: React.FC = () => {
 
   const logoutHandler = () => {
     localStorage.removeItem('token')
+    dispatch(searchActions.setSearch({ search: '' }))
+    dispatch(searchActions.setQuery({ query: {} }))
     dispatch(authActions.setAuth({ isAuth: false }))
     navigate(Route.Login)
   }
@@ -34,7 +49,6 @@ export const CustomHeader: React.FC = () => {
             <HeaderLogo />
           </Link>
           <Menu
-            defaultSelectedKeys={['1']}
             items={menuItems.map(menuItem => {
               const key = menuItem.id
 
@@ -44,6 +58,7 @@ export const CustomHeader: React.FC = () => {
               }
             })}
             mode={'horizontal'}
+            selectedKeys={selectedKeys}
             theme={'light'}
           />
         </Flex>
