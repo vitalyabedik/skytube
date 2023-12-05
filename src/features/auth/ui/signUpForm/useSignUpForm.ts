79b6701a1cb9b733c1eabcc9535983c6a29b0f09
Vitali Bedik <vitalya.bedik@gmail.com>
2023-12-05 20:35:30 +0300

    feat: change validation for SignUp form

--- src/features/auth/ui/signUpForm/useSignUpForm.ts
+++ src/features/auth/ui/signUpForm/useSignUpForm.ts
@@ -5,9 +5,7 @@ import { z } from 'zod'
 
 const loginSchema = z.object({
   email: z.string().email(),
-  // .regex(/[A-Z]/, 'Password must contain at least one uppercase letter'),
   login: z.string().min(1, 'Login is required!'),
-  // .min(6, 'Use 6 characters or more for your password!')
   password: z.string(),
 })
 
